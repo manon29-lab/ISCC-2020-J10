afcd33f854aa485dbb8732378e4e1b9d2f26fb72
manon29-lab <67279890+manon29-lab@users.noreply.github.com>
2020-07-07 21:24:32 +0200

    Add files via upload

---
+++ requete-select.sql
@@ -0,0 +1,5 @@
+SELECT * FROM produit
+SELECT * FROM produit WHERE nom LIKE "%t-shirt%"
+SELECT * FROM produit WHERE prix LIKE "%19.99%"
+SELECT * FROM produit WHERE nom LIKE "%t-shirt%"
+SELECT * FROM produit WHERE quantite LIKE "%5%"
